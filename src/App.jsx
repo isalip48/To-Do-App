204ade3128e9eb494a372b02d903b69ea57c971a
isali <isalinethra16@gmail.com>
2025-06-30 16:24:26 +0530

    Main Page Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Register from './components/register';
 import Home from './components/Home';
+import TodoPage from './components/MainPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} /> 
+        <Route path="/main" element={<TodoPage />} /> 
       </Routes>
     </Router>
   );
